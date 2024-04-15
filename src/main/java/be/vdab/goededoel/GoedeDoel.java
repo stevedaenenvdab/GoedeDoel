ee0631edf30e83a44a7272da568751a4d27cb987
cursistJavaHeverlee <steve.daenen@vdabcampus.be>
2024-04-15 15:39:06 +0200

    test7

--- src/main/java/be/vdab/goededoel/GoedeDoel.java
+++ src/main/java/be/vdab/goededoel/GoedeDoel.java
@@ -1,19 +1,31 @@
 package be.vdab.goededoel;
 
 import java.math.BigDecimal;
-
+/**
+ * Een <strong>goed doel</strong> waarvoor men geld inzamelt
+ * @author Joe Dalton
+ */
 public class GoedeDoel {
 	private final String naam;
 	private BigDecimal opbrengst;
-
+	/**
+	 * Maakt een GoedeDoel object
+	 * @param naam De naam van het goede doel
+	 */
 	public GoedeDoel(String naam) {
 		this.naam = naam;
 	}
-
+	/**
+	 * Geeft de naam terug
+	 * @return de naam
+	 */
 	public String getNaam() {
 		return naam;
 	}
-
+	/**
+	 * Geeft de opbrengst terug
+	 * @return de opbrengst
+	 */
 	public BigDecimal getOpbrengst() {
 		return opbrengst;
 	}
@@ -21,5 +33,9 @@ public class GoedeDoel {
 	@Override
 	public boolean equals(Object object) {
 		return object instanceof GoedeDoel ander && naam.equalsIgnoreCase(ander.naam);
+	}
+	@Override
+	public int hashCode() {
+		return this.naam.toUpperCase().hashCode();
 	}
 }
--- src/test/java/be/vdab/goededoel/GoedeDoelTest.java
+++ src/test/java/be/vdab/goededoel/GoedeDoelTest.java
@@ -41,4 +41,8 @@ class GoedeDoelTest {
     void doelVerschiltVanEenObjectMetEenAnderType() {
         assertThat(doel).isNotEqualTo(BigDecimal.ZERO);
     }
+    @Test
+    void gelijkeDoelGevenHebbenDezelfdeHashCode() {
+        assertThat(doel).hasSameHashCodeAs(new GoedeDoel(NAAM));
+    }
 }
